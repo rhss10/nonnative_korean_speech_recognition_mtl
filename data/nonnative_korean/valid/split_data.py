4ad1db85d302828dc827f90e7fc1f90f4f6adb0a
rhss10@naver.com <rhss10@naver.com>
2022-12-05 12:26:27 +0900

    deleted dataset not for use

--- data/nonnative_korean/valid/split_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-df = pd.read_csv('4_valid.csv')
-print(df)
-train, test = train_test_split(df, test_size=0.5, random_state=42, shuffle=True)
-
-test.to_csv('4.csv', index=False)
-train.to_csv('../../test/less_17/4.csv', index=False)
